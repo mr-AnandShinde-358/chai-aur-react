3428e54e6ea45ef26d267c6b7112a0f7570b05b6
mr-AnandShinde-358 <97289992+mr-AnandShinde-358@users.noreply.github.com>
2024-02-04 08:47:05 +0530

    self try context api with local storage project and redux -toolkit project

---
+++ 07trypr/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import AddTodo from './componets/AddTodo'
+import Todos from './componets/Todos'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
--- reduxtoolkittodo/src/componets/Todos.jsx
+++ reduxtoolkittodo/src/componets/Todos.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { removeTodo } from '../features/todo/todoSlice'
 
+
 function Todos() {
     const todos = useSelector(state=>state.todos)
     const dispatch = useDispatch()
