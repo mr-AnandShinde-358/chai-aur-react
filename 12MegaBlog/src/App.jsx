629bc6b9e45a40b986d58adff9c522a01cba5ed9
mr-AnandShinde-358 <97289992+mr-AnandShinde-358@users.noreply.github.com>
2024-02-12 11:28:35 +0530

    how to use react hook form in production

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,49 @@
+
+import { useState,useEffect } from 'react'
+
+import {useDispatch} from "react-redux"
+import './App.css'
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import Footer from './componets/Footer/Footer'
+import Header from './componets/Header/Header'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+
+  const [loading,setLoading] =useState(true);
+  const dispatch =useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+     if(userData){
+      dispatch(
+        login({userData})
+      )
+     }else{
+      dispatch(logout())
+     }
+      
+    })
+    .finally(
+      ()=>{setLoading(false)}
+    )
+  },[])
+  
+
+
+  return !loading ? (
+    <div className='min-h-full flex flex-wrap'>
+      <div className="w-full block">
+        <Header/>
+        <main>
+         <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):null
+}
+
+export default App
