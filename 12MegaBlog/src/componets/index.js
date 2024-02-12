5c363dea200aa0a7ea55c22d46f169bf3cc2cd75
mr-AnandShinde-358 <97289992+mr-AnandShinde-358@users.noreply.github.com>
2024-02-12 15:35:02 +0530

    adding form and slug values

--- 12MegaBlog/src/componets/index.js
+++ 12MegaBlog/src/componets/index.js
@@ -3,11 +3,21 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE"
+import Button from "./Button"
+import Input from "./Input"
+import Select from "./Select"
+
+
 
 export {
     Header,
     Footer,
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+   
+    
+
 }
